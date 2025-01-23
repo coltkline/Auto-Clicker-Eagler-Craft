8110460effa61e0c5da9fded2b7b9fbe9cc11878
coltkline <coltkline101@gmail.com>
2025-01-23 08:23:30 -0600

    Update autoclick.js

--- autoclick.js
+++ autoclick.js
@@ -1,7 +1,24 @@
-let cps = 0.5
+let cps = 0.5;
+let intervalTime = 1000 / cps;
+let intervalID = null;
 
 function click() {
-  ModAPI.clickMouse()
+    ModAPI.clickMouse();
 }
-let intervalTime = 1000 / cps;
-let intervalID = setInterval(click, intervalTime);
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === '[') {  // Change this to your desired activation key
+        if (intervalID === null) {
+            intervalID = setInterval(click, intervalTime);
+        }
+    }
+});
+
+document.addEventListener('keyup', function(event) {
+    if (event.key === '[') {  // Change this to your desired activation key
+        if (intervalID !== null) {
+            clearInterval(intervalID);
+            intervalID = null;
+        }
+    }
+});
